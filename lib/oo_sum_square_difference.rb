fb6bbef14775866184dd112a1d882c9dcbeb0327
drubaby <peryton6@gmail.com>
2018-11-27 09:30:31 -0500

    solution

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+
+  def initialize(number)
+    @number = number
+  end
+
+  def sum_of_squares(number)
+    array = (1..number).to_a
+    squares = array.map {|i| i*i}
+    sum = 0
+    squares.each {|i| sum+= i}
+    return sum
+  end
+
+  def square_of_sum(number)
+    array = (1..number).to_a
+    sum = 0
+    array.each {|i| sum+= i}
+    return sum * sum
+  end
+
+
+  def difference
+    square_of_sum(@number) - sum_of_squares(@number)
+  end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,22 @@
 # Implement your procedural solution here!
+
+
+def sum_of_squares(number)
+  array = (1..number).to_a
+  squares = array.map {|i| i*i}
+  sum = 0
+  squares.each {|i| sum+= i}
+  return sum
+end
+
+def square_of_sum(number)
+  array = (1..number).to_a
+  sum = 0
+  array.each {|i| sum+= i}
+  return sum * sum
+end
+
+
+def sum_square_difference(number)
+  square_of_sum(number) - sum_of_squares(number)
+end
